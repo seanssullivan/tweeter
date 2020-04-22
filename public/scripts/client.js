b6f3a879f6dca514d94db5445ddf0396ada7f5a0
Sean Sullivan <dev@seanmedia.ca>
2020-04-22 19:43:12 +0000

    Begin writing function to create html structure for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const sampleTweet =  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = (tweetObj) => {
+  const $tweet = $(`<article class="tweet">Hello world</article`);
+};
